e70ba564a78833929f21d8aadeb4ef944a600877
frank gimeno <frankgimeno3@gmail.com>
2024-07-30 23:58:48 +0200

    implementado correctamente cinzel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+// Importing Cinzel font from Google Fonts
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +17,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400..900&display=swap" rel="stylesheet" />
+      </head>
+      <body className={`${inter.className} font-cinzel`}>
+        {children}
+      </body>
     </html>
   );
 }
